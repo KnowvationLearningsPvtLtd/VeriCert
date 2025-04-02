8a3e3bdb8ed13a2d1229a99fe374770f0fd01581
npsimba <nandivelugupoojitha@gmail.com>
2025-04-02 14:47:39 +0530

    fix: Added email notification after login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,6 +4,7 @@ import User from '../models/userModel';
 import { generateAccessToken } from '../utils/jwt';
 import logger from '../utils/logger';
 import { registerSchema } from '../validations/registerSchema';
+import { sendEmail } from '../utils/emailService';
 
 const register = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -15,20 +16,27 @@ const register = async (req: Request, res: Response, next: NextFunction) => {
 
     const { username, email, password, role } = parsedData.data;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username,email, password: hashedPassword, role });
+    const newUser = new User({ username, email, password: hashedPassword, role });
 
     await newUser.save();
     logger.info(`User registered: ${email}`);
 
+    // Send Welcome Email
+    await sendEmail(
+      email,
+      'Welcome to VeriCert!',
+      `Hello ${username},\n\nYour account has been successfully registered.\n\nBest,\nVeriCert Team`
+    );
+
     res.status(201).json({ message: `User registered with email ${email}` });
   } catch (err) {
-    next(err); // Pass error to global error handler
+    next(err);
   }
 };
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const {  email, password } = req.body;
+    const { email, password } = req.body;
     const user = await User.findOne({ email });
 
     if (!user) {
@@ -40,11 +48,15 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    const token = generateAccessToken({ 
-      _id: user._id, 
-      role: user.role 
-    });
+    const token = generateAccessToken({ _id: user._id, role: user.role });
     logger.info(`User logged in: ${email}`);
+
+    // Send Login Notification Email
+    await sendEmail(
+      email,
+      'New Login Alert',
+      `Hello ${user.username},\n\nWe noticed a login to your account.\nIf this wasn't you, please reset your password.\n\nBest,\nVeriCert Team`
+    );
 
     res.status(200).json({ 
       token,
@@ -53,7 +65,8 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
         username: user.username,
         email: user.email,
         role: user.role,
-      } });
+      }
+    });
   } catch (err) {
     next(err);
   }
@@ -68,7 +81,3 @@ const logout = (req: Request, res: Response, next: NextFunction) => {
 };
 
 export { register, login, logout };
-
-
-
-
